3085c7f1fc5fde458524bc023f19d44a0a39336b
hhhh <hhhh@gmail.com>
2020-11-05 18:07:37 -0600

    encript

---
+++ Encrypt/Encryp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Encryp
+{
+    public class Davici
+    {
+        public string TextEncrypt(string clave, string text){
+            
+            List<char> ABCedario = ("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ").ToCharArray().ToList();
+            char[] Col = clave.ToUpper().ToCharArray(); //clave
+            char[] Row  = text.ToUpper().ToCharArray(); //texto
+
+            string Encriptation = "";
+            int j = 0;
+
+            for (int i = 0; i < Row.Length; i++)
+            {
+
+                int ROW = ABCedario.IndexOf(Row[i]); //texto
+                int COL = ABCedario.IndexOf(Col[j]); //clave
+
+                if(ROW < 0 || COL < 0){
+                    Encriptation += Row[i];
+                    continue;
+                }
+
+                int NewLetter =  ROW + COL;
+
+                if(NewLetter > 26) 
+                    NewLetter = NewLetter - 27;
+                
+
+                Encriptation += ABCedario.ElementAt(NewLetter);
+
+                j++;
+                if(j > Col.Length-1) j = 0;
+            }
+            
+            //iwvevogta uavlm qmmoñln cmfet. kj xb woy. <-resultado esperado
+            //IWVEVOGTA UAVLM QMMOÑLN CMFET. KJ XB WOY.
+            return Encriptation;
+        }
+
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Encryp;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Digite el text:");
+            string clave = Console.ReadLine();
+
+            Console.WriteLine("Digite el text:");
+            string text = Console.ReadLine();*/
+
+            Davici decrypt = new Davici();
+
+            Console.WriteLine(decrypt.TextEncrypt("PIKACHU","Sometimes later becomes never. Do it now."));
+        }
+    }
+}
